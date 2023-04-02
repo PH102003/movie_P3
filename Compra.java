222db60901662d0334afabec9735658dbea3b88e
Pedro Henrique <pedroh.rj2003@hotmail.com>
2023-04-02 18:35:22 -0300

    atividade quase feita

--- Compra.java
+++ Compra.java
@@ -1,7 +1,49 @@
 package classes;
 
 public class Compra {
+    private double valorPipoca;
+    private double CocaCola;
+    private int numsBilhetes;
     
+    public Compra(String valorPipoca, String cocaCola, int numsBilhetes) {
+        this.valorPipoca = valorPipoca;
+        CocaCola = cocaCola;
+        this.numsBilhetes = numsBilhetes;
+    }
+
+    public String getValorPipoca() {
+        return valorPipoca;
+    }
+    public void setValorPipoca(String valorPipoca) {
+        this.valorPipoca = valorPipoca;
+    }
+    public String getCocaCola() {
+        return CocaCola;
+    }
+    public void setCocaCola(String cocaCola) {
+        CocaCola = cocaCola;
+    }
+    public int getNumsBilhetes() {
+        return numsBilhetes;
+    }
+    public void setNumsBilhetes(int numsBilhetes) {
+        this.numsBilhetes = numsBilhetes;
+    }
+    
+    //metodos
+    public double pegarItens(int quantiCoca, int quantiPipoca){
+        double resultadoItens = valorPipoca * quantiPipoca + CocaCola * quantiCoca;
+        return resultadoItens;
+    }
+    public double valorBilhete(int quantidadeBilhetes){
+        double soma = quantidadeBilhetes * 20;
+        return soma;
+
+    }  
+    public double valorFinal(){
+        double somaTotal = pegarItens(numsBilhetes, numsBilhetes) + valorBilhete(numsBilhetes);
+        return somaTotal;
+    }
 }
 /*
  A classe compra é a classe que contém as compras que podem ser realizadas,  a
